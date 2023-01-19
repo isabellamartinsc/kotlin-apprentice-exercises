d5d1ab7818ba588911c1c57cba5cf1283e845854
isabella-cruz <isabella.cruz@ifood.com.br>
2023-01-19 20:21:04 -0300

    Mini Exercises charapter 1 - 3. types and operations

--- src/main/kotlin/sectionone/miniexercises/MiniExercises.kt
+++ src/main/kotlin/sectionone/miniexercises/MiniExercises.kt
@@ -1,6 +1,8 @@
 package sectionone.miniexercises
 
 fun main() {
+    //Expressions, variables and constants
+
 /*1. Declare a constant of type Int called myAge and set it to your age.*/
     val myAge: Int = 25
     println("myAge = $myAge")
@@ -28,4 +30,21 @@ After all of these operations, what’s the answer?*/
     answer = answer shr 3
     println("answer is: $answer")
 
+    //Types and Operations
+
+/*1. Create a constant called age1 and set it equal to 42. Create a constant called age2 and set it equal to 21.
+Check using Control+Shift+P that the type for both has been inferred correctly as Int.*/
+    val age1 = 42
+    val age2 = 21
+    //answer: type inferred correctly.
+
+/*2. Create a constant called avg1 and set it equal to the average of age1 and age2 using the naive operation
+(age1 + age2) / 2. Use Control+Shift+P to check the type and check the result of avg1. Why is it wrong?*/
+    val avg1 = (age1 + age2) / 2
+    println("avg1: $avg1")
+
+/*3. Correct the mistake in the above exercise by converting age1 and age2 to type Double in the formula.
+Use Control+Shift+P to check the type and check the result of avg1. Why is it now correct?*/
+    val avg11: Double = (age1.toDouble() + age2.toDouble()) / 2
+    println("avg1: $avg11")
 }
