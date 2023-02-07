32df50360852ebd0f0f6b8df029bc57fd58aa5e5
isabella-cruz <isabella.cruz@ifood.com.br>
2023-02-07 20:51:28 -0300

    Mini exercises section 4

--- src/main/kotlin/sectionone/miniexercises/MiniExercisesSection4.kt
+++ src/main/kotlin/sectionone/miniexercises/MiniExercisesSection4.kt
@@ -1,5 +1,7 @@
 package sectionone.miniexercises
 
+import java.util.*
+
 fun main() {
 // Comparison operators
 /* 1. Create a constant called myAge and set it to your age. Then, create a constant named isTeenager that uses
@@ -33,4 +35,30 @@ author */
 
     val readerBeforeAuthor = reader < author
     println("readerBeforeAuthor? $readerBeforeAuthor")
+
+// Loops
+/* 1. Create a variable named counter and set it equal to 0. Create a while loop with the condition counter < 10
+which prints out counter is X (where X is replaced with counter value) and then increments counter by 1. */
+
+    var counter = 0
+
+    while (counter < 10) {
+        println("counter is $counter")
+        counter += 1
+    }
+
+/* 2. Create a variable named counter and set it equal to 0. Create another variable named roll and set it equal to 0.
+Create a do-while loop. Inside the loop, set roll equal to Random().nextInt(6) which means to pick a random number
+between 0 and 5. Then increment counter by 1.
+Finally, print After X rolls, roll is Y where X is the value of counter and Y is the value of roll. Set the loop
+condition such that the loop finishes when the first 0 is rolled. */
+
+    var roll = 0
+
+    do {
+        roll = Random().nextInt(6)
+        counter += 1
+        println("After $counter rolls, roll is $roll where $counter is the value of counter and $roll is the value of roll")
+    } while (roll > 0)
+
 }
