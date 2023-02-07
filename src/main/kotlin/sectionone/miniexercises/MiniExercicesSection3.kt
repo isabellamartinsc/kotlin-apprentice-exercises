f97f112a70f53dcfe12ad0722de16b32c8ae8d5d
isabella-cruz <isabella.cruz@ifood.com.br>
2023-02-07 19:48:50 -0300

    Strings mini exercises - Types and operations

--- src/main/kotlin/sectionone/miniexercises/MiniExercicesSection3.kt
+++ src/main/kotlin/sectionone/miniexercises/MiniExercicesSection3.kt
@@ -19,4 +19,34 @@ separated by a space. */
 string introducing yourself. For example, it could read: "Hello, my name is Joe Howard.". */
     val myDetails = "Hello, my name is $fullName."
     println(myDetails)
+
+
+//Pairs and Triples
+
+/*1. Declare a constant Triple that contains three Int values. Use this to represent a date (month, day, year).*/
+
+    val date: Triple<Int, Int, Int> = Triple(first = 2, second = 10, third = 2023)
+    println(date)
+
+/*2. Extract the values in the triple into three constants named month, day and year.*/
+
+    val month = date.second
+    val day = date.first
+    val year = date.third
+
+    println("Format date in Brazil is: $day/$month/$year.")
+
+/*3. In one line, read the month and year values into two constants. You’ll need to employ the underscore to
+ignore the day.*/
+
+    var (_, monthConst, yearConst) = date
+
+/*4.Since the values inside Pairs and Triples cannot be modified, you will need to extract the values from them,
+make any modifications you want, and then create a new Pair or Triple. Using the values you extracted in step three,
+modify the month value and create a new Pair containing the modified month along with the unmodified year.*/
+
+    monthConst = 8
+    val updateDate = Pair(monthConst, year)
+    println(updateDate)
+
 }
